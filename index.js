b69daf3eb55a4c8f1992ad1b2f0a207d71602504
KrzysztofSobota <ksobota1981@gmail.com>
2019-07-29 21:22:38 +0200

    Usability corrections
    Some usability changes:
    - text in paragraphes without 'justify' align,
    - remove underline in 'span' text,
    - a little colors contrast correction,
    - anchor text replacing in 'aside'

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 'use strict'
 
-window.addEventListener("DOMContentLoaded", PiTest);
+// window.addEventListener("DOMContentLoaded", PiTest);
 
 function PiTest() {
-  const canvas = document.getElementById('myCanvas');
-  const a = canvas.width; // square edge value
-  const r = canvas.width/2; // circle radius value
-  
-/* Getting table cell names for the variables */
-  const pointsInside = document.querySelector('#number1');
-  const percent = document.querySelector('#number2');
-  const piValue = document.querySelector('#PiResult');  
+  const canvas = document.querySelector('#myCanvas');
+  let a = canvas.width; // square edge value
+  let r = canvas.width/2; // circle radius value
   
 /* Choosing sample size for test */
-  let valueSelected= document.querySelector('#valueList');
+  let valueSelected = document.querySelector('#valueList');
   
   function selectedValues() {
     let listValue = valueSelected.value;
-    let testNumber = parseInt(listValue);
+    let testNumber = Number(listValue);
       
     return testNumber;       
   }
   
-  let putNumber = selectedValues();
-  
+  let putNumber = selectedValues();  
   valueSelected.addEventListener('change', selectedValues);
 
   
@@ -32,26 +26,28 @@ function PiTest() {
   function calculate(putNumber) {
     let coordinates = [];
     let j = 0;
+ 
+    const pointsInside = document.querySelector('#number1');
+    const percent = document.querySelector('#number2');
+    const piValue = document.querySelector('#PiResult');
 
     for (let i = 1; i <= putNumber; i++) {
       /* Double using Math.round() because x nad y values for point (x,y) must be calculate independent!! */
       let x = Math.round((Math.random()) * a);
       let y = Math.round((Math.random()) * a);
-      let equation = Math.pow(x-r, 2) + Math.pow(y-r, 2);
-      let point = Math.round(Math.sqrt(equation));
-      
       coordinates.push(x,y);
-      
-/* Checking if point is inside the circle (including circle edge) */
+
+      let squareEquation = Math.pow(x-r, 2) + Math.pow(y-r, 2);
+      let point = Math.round(Math.sqrt(squareEquation));
       
         if (point <= r) {
           j++;
         }
-      
-/* Calculating results on the table */
+     
+  /* Calculating results on the table */
       pointsInside.textContent = j;
       percent.textContent = (j / putNumber * 100).toFixed(1);
-      piValue.textContent = (4 * (j/i)).toFixed(2);
+      piValue.textContent = (4 * (j / i)).toFixed(2);
     }
       
     return coordinates;
@@ -60,15 +56,16 @@ function PiTest() {
   let xyArray = calculate(putNumber);
 
 /*** Drawing point into the canvas (graphical parts of the test) ***/
+const ctx = canvas.getContext('2d');
   function draw() {
-    const ctx = canvas.getContext('2d');
+    
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     // taking x and y from the array
     for (let i = 0; i < (xyArray.length / 2); i++) {
-      let X = xyArray[2*i];
-      let Y = xyArray[2*i+1]; 
+      let X = xyArray[2 * i];
+      let Y = xyArray[2 * i+ 1]; 
       
 /* (0,0) point is moving to the center of canvas */
       let equation2 = Math.pow(X-r, 2) + Math.pow(Y-r, 2);
@@ -87,10 +84,10 @@ function PiTest() {
       ctx.fillRect(X, Y, 1, 1);
     }
     
-    window.requestAnimationFrame(draw);
+    // window.requestAnimationFrame(draw);
   }
   
-  draw(xyArray);
+  draw();
 }
   
 let btn = document.querySelector('#testing');
@@ -100,11 +97,11 @@ btn.addEventListener('click', PiTest);
 
 let showHideButton = document.querySelector('#savespace');
 
-function ShowHideText(showHideButton) {
+function ShowHideText() {
   let hiddenText = document.querySelector('.description');
   
   if (hiddenText.style.display === 'none') {
-    hiddenText.style.display = 'block';
+    hiddenText.style.display = 'flex';
   } else {
     hiddenText.style.display = 'none';
   }
